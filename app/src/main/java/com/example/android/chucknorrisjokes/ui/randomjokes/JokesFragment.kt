ab0c0bb54c879ffd09994cf94f89ea6a1554c5bf
1n7 <im.in.cube@gmail.com>
2021-08-01 23:36:24 +0300

    [FIX] full work without databinding in JokesFragment.kt Keyboard auto hide after button reload pressed

--- app/src/main/java/com/example/android/chucknorrisjokes/ui/randomjokes/JokesFragment.kt
+++ app/src/main/java/com/example/android/chucknorrisjokes/ui/randomjokes/JokesFragment.kt
@@ -1,11 +1,13 @@
 package com.example.android.chucknorrisjokes.ui.randomjokes
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.LifecycleOwner
 import com.example.android.chucknorrisjokes.databinding.FragmentJokesBinding
 
@@ -23,6 +25,17 @@ class JokesFragment : Fragment(), LifecycleOwner {
         binding.setLifecycleOwner { this.lifecycle }
         binding.viewModel = viewModel
         binding.jokesList.adapter = JokesAdapter()
+        binding.button.setOnClickListener { loadAndHideKeyboard(it, viewModel) }
         return binding.root
+    }
+
+    private fun loadAndHideKeyboard(view: View, viewModel: JokesViewModel) {
+        viewModel.reload()
+        view.hideKeyboard()
+    }
+
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 }
